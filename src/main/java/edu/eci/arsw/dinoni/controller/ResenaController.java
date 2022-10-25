653eb131dc204d6452696f5c0d1a641c0e1dd4bf
NorbeyC <norbey.cardona@mail.escuelaing.edu.co>
2022-10-25 13:47:35 -0500

    seccion  de comentarios

--- src/main/java/edu/eci/arsw/dinoni/controller/ResenaController.java
+++ src/main/java/edu/eci/arsw/dinoni/controller/ResenaController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import edu.eci.arsw.dinoni.service.ResenaService;
 import edu.eci.arsw.dinoni.model.Resena;
@@ -37,6 +39,7 @@ public class ResenaController {
         Resena resena = resenaService.getResenaById(id).get();
         return new ResponseEntity<>(resena, HttpStatus.ACCEPTED);
     }
+
 
     @PostMapping("/saveResena")
     public ResponseEntity<?> saveResena(@RequestBody Resena resena){
