28d27764dd292b01c150093d374d940ed417a501
NorbeyC <norbey.cardona@mail.escuelaing.edu.co>
2022-10-24 20:33:40 -0500

    update html

--- src/main/java/edu/eci/arsw/dinoni/DinoniApplication.java
+++ src/main/java/edu/eci/arsw/dinoni/DinoniApplication.java
@@ -2,12 +2,13 @@ package edu.eci.arsw.dinoni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.eci.arsw.dinoni"})
 public class DinoniApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DinoniApplication.class, args);
 	}
-
 }
