087bf0038ff027bc03eb4520fd55d77a0f9d6d9f
unknown <diego.leon-ba@labinfo.is.escuelaing.edu.co>
2022-10-26 09:00:47 -0500

    Se agrega la funcionalidad Registrar Comentario

--- src/main/java/edu/eci/arsw/dinoni/controller/ResenaController.java
+++ src/main/java/edu/eci/arsw/dinoni/controller/ResenaController.java
@@ -8,11 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import edu.eci.arsw.dinoni.service.ResenaService;
 import edu.eci.arsw.dinoni.model.Resena;
@@ -65,6 +67,21 @@ public class ResenaController {
         }
         resenaService.updateResena(id, resena);
         return new ResponseEntity<>("Se ha actualizado correctamente ",HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("nuevoComentario")
+    public ModelAndView nuevoComentario(){
+        ModelAndView mav = new ModelAndView();
+        Resena resena = new Resena();
+        mav.setViewName("nuevoComentario");
+        mav.addObject("resena",resena);
+        return mav;
+    }
+    
+    @PostMapping("/saveComentario")
+    public String saveComentario(@ModelAttribute("resena") Resena resena){
+        resenaService.saveResena(resena);
+        return "redirect:/tienda/viewProductos";
     }
     
 }
